61172384cc9acde8bdfdcb2b45c07b647d48f323
Amanda Cameron <git@amanda.darkdna.net>
2014-05-13 17:10:22 -0400

    Add desktop backgrounds. (One for now.)

---
+++ glox/lib/res/backgrounds/cats.background.lua
@@ -0,0 +1,43 @@
+--65C1878C53BFCE--
+Name: metadata
+--65C1878C53BFCE--
+Name: Cats
+Creater: jgs (http://ascii-art.com)
+Copyright: 1996-2001 JGS
+--65C1878C53BFCE--
+Name: computer
+Width: 32
+Type: ASCII
+--65C1878C53BFCE--
+Zzzzz  |\      _,,,--,,_     
+       /,`.-'`'   ._  \-;;,_ 
+      |,4-  ) )_   .;.(  `'-'
+     '---''(_/._)-'(_\_)     
+--65C1878C53BFCE--
+Name: pocket
+Width: 9
+Type: ASCII
+--65C1878C53BFCE--
+ /\___/\
+ )     (
+=\     /=
+  )   (
+ /     \
+ )     (
+/       \
+\       /
+ \__ __/
+    ))
+   //
+  ((
+   \)
+--65C1878C53BFCE--
+Name: turtle
+Width: 32
+Type: ASCII
+--65C1878C53BFCE--
+Zzzzz  |\      _,,,--,,_     
+       /,`.-'`'   ._  \-;;,_ 
+      |,4-  ) )_   .;.(  `'-'
+     '---''(_/._)-'(_\_)   
+--65C1878C53BFCE--
--- glox/lib/widgets/glox-desktop.lua
+++ glox/lib/widgets/glox-desktop.lua
@@ -7,8 +7,43 @@ function Widget:init(app, w, h)
   self.agui_widget.bg = 'glox-desktop-bg'
 
   self.app = app
+
+  app:subscribe('glox.settings.commit', function()
+    self.background = new('kvio-bundle', app.settings:get_background())
+    self.background:load()
+  end)
 end
 
 function Widget:draw(c)
 	c:clear()
+
+  if self.background then
+    filename = "computer"
+
+    if pocket then
+      filename = "pocket"
+    elseif turtle then
+      filename = "turtle"
+    end
+    
+    if not self.background:exists(filename) then
+      filename = "computer"
+    end
+
+    if c.is_colour and self.background:exists("adv-" .. filename) then
+      filename = "adv-" .. filename
+    end
+
+    if self.background:get_prop(filename, "Type") == "ASCII" then
+      local f = self.background:open(filename, "r")
+      local width = tonumber(self.background:get_prop(filename, "Width"))
+
+      for y=1,#f.contents do
+        c:move(c.width / 2 - width / 2, (c.height / 2 - #f.contents / 2) + y)
+        c:write(f.contents[y])
+      end
+
+      f:close()
+    end
+  end
 end
