6db02ab71bf093775cae4b58b5c866de2118f8a7
Amanda Cameron <git@amanda.darkdna.net>
2014-05-15 15:02:19 -0400

    Finish up adding keybaord input to switch apps.

--- glox/lib/mono-theme.lua
+++ glox/lib/mono-theme.lua
@@ -19,8 +19,8 @@
     ['glox-menubar-drawer-bg'] = 'glox-menubar-bg',
 
     -- Drawer
-    ['glox-drawer-title-fg'] = 'white',
-    ['glox-drawer-title-bg'] = 'black',
+    ['glox-drawer-title-fg'] = 'black',
+    ['glox-drawer-title-bg'] = 'white',
     
     ['glox-drawer-plugin-fg'] = 'black',
     ['glox-drawer-plugin-bg'] = 'white',
@@ -28,8 +28,8 @@
     ['glox-drawer-minimised-fg'] = 'black',
     ['glox-drawer-minimised-bg'] = 'white',
     
-    ['glox-drawer-handle-fg'] = 'white',
-    ['glox-drawer-handle-bg'] = 'black',
+    ['glox-drawer-handle-fg'] = 'black',
+    ['glox-drawer-handle-bg'] = 'white',
 
     -- Menu Plugins.
     ['glox-search-indexing-fg'] = 'glox-menubar-fg',
